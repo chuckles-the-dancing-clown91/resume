261060938c8b9a035b5a107b26e073286a2afd00
cwilson <charles.l.wilson1028@gmail.com>
2022-06-16 21:15:57 -0400

    removed sec key

--- resume/settings.py
+++ resume/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure-je+f0#ld=*mm=y*bw+fewb*1ygc&pmtent&tlj9-+4@a@!(nrt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
